c62690c1cfa1a2be2c2309df664eb7ee2d07774b
vt156 <vt156156@gmail.com>
2020-02-27 12:48:33 +0530

    Update squid config

--- install.sh
+++ install.sh
@@ -13,9 +13,23 @@ echo "
 auth_param basic program /usr/lib/squid3/basic_ncsa_auth /etc/squid/passwords
 auth_param basic realm proxy
 acl authenticated proxy_auth REQUIRED
+
+# Privacy
+via off
+forwarded_for off
+request_header_access Via deny all
+request_header_access Forwarded-For deny all
+request_header_access X-Forwarded-For deny all
+
+acl SSL_ports port 443
+acl Safe_ports port 443
+acl Safe_ports port 80
+acl CONNECT method CONNECT
+http_access deny !Safe_ports
+http_access deny CONNECT !SSL_ports
 http_access allow authenticated
 
-http_port 3128
+http_port 3768
 " > /etc/squid/squid.conf
 
 # use expect to create user and set password for said user
